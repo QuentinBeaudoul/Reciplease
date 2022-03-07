f61cda7438d2b2cf8b628133d8816171980c7fe5
Quentin Beaudoul <qbeaudoul@ikomobi.com>
2022-03-07 17:45:08 +0100

    Appels WS

--- Frameworks/RSearch/SearchManager.swift
+++ Frameworks/RSearch/SearchManager.swift
@@ -8,15 +8,55 @@
 import Foundation
 import UIKit
 import RExtension
+import RNetwork
 
-public final class SearchManager {
+
+protocol SearchManagerProtocol {
+    func fetchRecipes(search: RequestParams, completion: @escaping (Result<ResponseContainer?, Error>) -> Void)
+    func fetchNextPage(withUrl url: String, completion: @escaping (Result<ResponseContainer?, Error>) -> Void)
+}
+
+public final class SearchManager: SearchManagerProtocol {
     
     public static let shared = SearchManager()
-    private init() {}
+
+    private let networkManager: NetworkManagerProtocol
+
+    private init(networkManager: NetworkManagerProtocol = NetworkManager.shared) {
+        self.networkManager = networkManager
+    }
     
     public func getViewController() -> UIViewController {
         let viewController = SearchViewController.makeFromStoryboard(in: Bundle(for: Self.self))
         viewController.tabBarItem = UITabBarItem(title: "", image: UIImage(systemName: "magnifyingglass.circle"), selectedImage: UIImage(systemName: "magnifyingglass.circle.fill"))
         return viewController
+    }
+
+    func fetchRecipes(search: RequestParams, completion: @escaping (Result<ResponseContainer?, Error>) -> Void) {
+
+        let url = Constantes.searchUrl
+        let params = search.buildParameters()
+
+        handleResult(url: url, params: params, completion: completion)
+    }
+
+    func fetchNextPage(withUrl url: String, completion: @escaping (Result<ResponseContainer?, Error>) -> Void) {
+        handleResult(url: url, params: nil, completion: completion)
+    }
+
+    private func handleResult(url: String, params: [String: Any]?, completion: @escaping (Result<ResponseContainer?, Error>) -> Void) {
+
+        networkManager.fetchData(url: url, headers: nil, parameters: params, parser: ResponseContainer.self) { result in
+            switch result {
+            case .success(let container):
+                if let container = container {
+                    completion(.success(container))
+                } else {
+                    completion(.success(nil))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
     }
 }
--- Frameworks/RSearch/SearchViewModel.swift
+++ Frameworks/RSearch/SearchViewModel.swift
@@ -8,5 +8,53 @@
 import Foundation
 
 class SearchViewModel {
-    
+
+    private let manager: SearchManagerProtocol
+
+    init(manager: SearchManagerProtocol = SearchManager.shared) {
+        self.manager = manager
+    }
+
+    private(set) var container: ResponseContainer?
+
+    func fetchRecipes(keywords: String, completion: @escaping (Result<Void, Error>) -> Void) {
+        let request = RequestParams(search: keywords)
+
+        manager.fetchRecipes(search: request) { result in
+            switch result {
+
+            case .success(let container):
+
+                if let container = container {
+                    self.container = container
+                    completion(.success())
+                }
+            case .failure(let error):
+
+                completion(.failure(error))
+            }
+        }
+    }
+
+    func fetchNextPage(completion: @escaping (Result<Void, Error>) -> Void) {
+
+        manager.fetchNextPage(withUrl: getNextPage()) { result in
+            switch result {
+                
+            case .success(let container):
+
+                if let container = container {
+                    self.container = container
+                    completion(.success())
+                }
+            case .failure(let error):
+
+                completion(.failure(error))
+            }
+        }
+    }
+
+    func getNextPage() -> String {
+        return container?.links?.nextPage ?? ""
+    }
 }
